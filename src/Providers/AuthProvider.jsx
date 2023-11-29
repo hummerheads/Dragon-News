f0d10bd080c25c5a2bfd67b6f954c963b135a096
Abdullah Al Mamun <101423827+hummerheads@users.noreply.github.com>
2023-11-30 04:58:18 +0600

    Register With Email, Password, Photo Link

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContext } from "react";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signOut } from "firebase/auth";
 import app from "../firebase/firebase.config";
 
 export const AuthContext = createContext(null);
@@ -9,15 +9,30 @@ const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
 
-    const [user, SetUser] = useState(null);
+    const [user, setUser] = useState(null);
 
     const createUser = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password); 
     }
 
+    const logOut = () => {
+        return signOut(auth);
+    }
+
+    useEffect( () =>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser =>{
+            console.log('Auth state changed', currentUser);
+            setUser(currentUser);
+        });
+        return () => {
+            unSubscribe();
+        }
+    } ,[])
+
     const AuthInfo = {
         user,
-        createUser
+        createUser,
+        logOut
     }
 
     return (
